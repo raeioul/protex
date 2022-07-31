a5d1e3c885f7420984b0d5d99722db9d0d844e48
raeioul <raeioul@gmail.com>
2022-07-31 04:10:52 -0400

    Fix emails

--- app/Http/Controllers/Admin/OperationsController.php
+++ app/Http/Controllers/Admin/OperationsController.php
@@ -157,7 +157,7 @@ class OperationsController extends Controller
         $operation = Operation::create($input);
                 
         
-        $emails = ['raeioul@gmail.com'];
+        $emails = ['importaciones@protex.com.bo', 'contabilidad.cbba@protex.com.bo', 'protextex@gmail.com'];
         $input['created_at'] = $operation->created_at;
         $input['proveedor']= Provider::findOrFail($input['proveedor'])->name;
         $input['productosCantidades']= $productosCantidades;
